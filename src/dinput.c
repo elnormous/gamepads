037e55b407807ee5da8ba942ffd9f77f1ad5f8d3
Elviss Strazdins <elviss@elviss.lv>
2018-02-18 22:42:41 +0200

    Start implementing DirectInput

--- src/dinput.c
+++ src/dinput.c
@@ -2,9 +2,26 @@
 //  gamepads
 //
 
+#define DIRECTINPUT_VERSION 0x0800
+#include <dinput.h>
 #include "input.h"
+
+typedef struct InputDI
+{
+    IDirectInput8W* directInput;
+} InputDI;
 
 int gpInputInit(Input* input)
 {
+    InputDI* inputDI = malloc(sizeof(InputDI));
+    input->opaque = inputDI;
+
+    return 1;
+}
+
+int gpInputDestroy(Input* input)
+{
+    free(input->opaque);
+
     return 1;
 }
