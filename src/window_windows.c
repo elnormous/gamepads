e369193941c33ac9b59b2654eb2f8bc22438a9d3
Elviss Strazdins <elviss@elviss.lv>
2018-03-22 17:13:40 +0200

    Create Window on Windows

--- src/window_windows.c
+++ src/window_windows.c
@@ -8,13 +8,69 @@
 
 typedef struct GPWindowWindows
 {
+    ATOM windowClass;
     HWND window;
 } GPWindowWindows;
+
+static LRESULT CALLBACK windowProc(HWND window, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    return DefWindowProcW(window, msg, wParam, lParam);   
+}
+
+static const LPCWSTR WINDOW_CLASS_NAME = L"gamepads";
 
 int gpWindowInit(GPWindow* window)
 {
     GPWindowWindows* windowWindows = malloc(sizeof(GPWindowWindows));
     window->opaque = windowWindows;
+
+    HINSTANCE instance = GetModuleHandleW(NULL);
+
+    WNDCLASSEXW wc;
+    wc.cbSize = sizeof(wc);
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+    wc.lpfnWndProc = windowProc;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hInstance = instance;
+    // Application icon should be the first resource
+    //wc.hIcon = LoadIconW(instance, MAKEINTRESOURCEW(101));
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = static_cast<HBRUSH>(GetStockObject(COLOR_WINDOW));
+    wc.lpszMenuName = NULL;
+    wc.lpszClassName = WINDOW_CLASS_NAME;
+    wc.hIconSm = NULL;
+
+    windowWindows->windowClass = RegisterClassExW(&wc);
+    if (!windowWindows->windowClass)
+    {
+        fprintf(stderr, "Failed to register window class\n");
+        return 0;
+    }
+
+    DWORD windowStyle = WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX | WS_CLIPSIBLINGS | WS_BORDER | WS_DLGFRAME | WS_THICKFRAME | WS_GROUP | WS_TABSTOP | WS_SIZEBOX | WS_MAXIMIZEBOX;
+
+    int x = CW_USEDEFAULT;
+    int y = CW_USEDEFAULT;
+
+    DWORD windowExStyle = WS_EX_APPWINDOW;
+
+    int width = CW_USEDEFAULT;
+    int height = CW_USEDEFAULT;
+    int width = windowRect.right - windowRect.left;
+    int height = windowRect.bottom - windowRect.top;
+
+    windowWindows->window = CreateWindowExW(windowExStyle, WINDOW_CLASS_NAME, L"Gamepads", windowStyle,
+                                            x, y, width, height, NULL, NULL, instance, NULL);
+
+    if (!window)
+    {
+        fprintf(stderr, "Failed to create window\n");
+        return 0;
+    }
+
+    ShowWindow(windowWindows->window, SW_SHOW);
+    SetWindowLongPtr(windowWindows->window, GWLP_USERDATA, (LONG_PTR)windowWindows);
 
     return 1;
 }
@@ -24,6 +80,9 @@ int gpWindowDestroy(GPWindow* window)
     if (window->opaque)
     {
         GPWindowWindows* windowWindows = (GPWindowWindows*)window->opaque;
+
+        if (windowWindows->window) DestroyWindow(windowWindows->window);
+        if (windowWindows->windowClass) UnregisterClassW(WINDOW_CLASS_NAME, GetModuleHandleW(NULL));
 
         free(windowWindows);
     }
